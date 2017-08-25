078d2290a3dcd134056bdd641a68776f02aa294a
Y-Wakuta <prsprivatestock@i.softbank.jp>
2017-08-26 04:31:10 +0900

    フォルダを再度整理

--- AlbaAnalysis/AlbaAnalysis/AdditionalPanel/ConfigDialog.cs
+++ AlbaAnalysis/AlbaAnalysis/AdditionalPanel/ConfigDialog.cs
@@ -7,11 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.ComponentModel;
+
+using AlbaAnalysis.Entity;
 
 namespace AlbaAnalysis {
     public partial class ConfigDialog : Form {
+
+        SerialEntity serial = new SerialEntity();
+
         public ConfigDialog() {
             InitializeComponent();
+            var result = GetNamesOrdered();
+        }
+
+        public string[] GetNamesOrdered() {
+            var fields = typeof(SerialEntity).GetFields();
+            var dispOrderAttribute = Attribute.GetCustomAttributes(typeof(SerialEntity), false) as DisplayOrderAttribute[];
+            return dispOrderAttribute.OrderBy(r => r.order).Select(r => r.name).ToArray();
         }
     }
 }
