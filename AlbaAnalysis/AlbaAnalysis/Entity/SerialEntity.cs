49b415e0af2db038decda402772475007539cdc8
O365\u241408a <rockoffifth@gmail.com>
2018-03-08 00:43:11 +0900

    Entityクラスを分割

--- AlbaAnalysis/AlbaAnalysis/Entity/SerialEntity.cs
+++ AlbaAnalysis/AlbaAnalysis/Entity/SerialEntity.cs
@@ -9,98 +9,143 @@ using AlbaAnalysis.Routine;
 
 namespace AlbaAnalysis.Entity
 {
-    /// <summary>
-    /// ここに新しい要素を追加するときは、database.sqlにも項目を追加してください
-    /// </summary>
-    public class SerialEntity : INotifyPropertyChanged
+    public class FirstEntity : BaseEntity
     {
-
-        public SerialEntity Clone() {
-            return (SerialEntity)MemberwiseClone();
-        }
-
-        #region column
-        #region migrated
-        public string Time {
-            get { return _time; }
+        public double AirSpeedTime {
+            get { return _airSpeed_time; }
             set {
-                _time = value;
-                onPropertyChanged(nameof(Time));
+                _airSpeed_time = value;
+                onPropertyChanged(nameof(AirSpeedTime));
             }
         }
-        private string _time = "0";
+        public double _airSpeed_time;
 
-        public string Cadence {
-            get { return _cadence; }
-            set {
-                _cadence = value;
-                onPropertyChanged(nameof(Cadence));
-            }
-        }
-        public string _cadence = "0";
-
-        public string MpuRoll {
-            get { return _mpuRoll; }
-            set {
-                _mpuRoll = value;
-                onPropertyChanged(nameof(MpuRoll));
-            }
-        }
-        private string _mpuRoll = "0";
-
-        public string MpuPitch {
-            get { return _mpuPitch; }
-            set {
-                _mpuPitch = value;
-                onPropertyChanged(nameof(MpuPitch));
-            }
-        }
-        public string _mpuPitch = "0";
-
-        public string MpuYaw {
-            get { return _mpuYaw; }
-            set {
-                _mpuYaw = value;
-                onPropertyChanged(nameof(MpuYaw));
-            }
-        }
-        public string _mpuYaw = "0";
-
-        public string AirSpeed {
+        public double AirSpeed {
             get { return _airSpeed; }
             set {
                 _airSpeed = value;
                 onPropertyChanged(nameof(AirSpeed));
             }
         }
-        public string _airSpeed = "0";
+        public double _airSpeed;
 
-        public string Sonar { get; set; } = "0";
+        public double CadenceTime {
+            get { return _cadence_time; }
+            set {
+                _cadence_time = value;
+                onPropertyChanged(nameof(CadenceTime));
+            }
+        }
+        public double _cadence_time;
+
+        public double Cadence {
+            get { return _cadence; }
+            set {
+                _cadence = value;
+                onPropertyChanged(nameof(Cadence));
+            }
+        }
+        public double _cadence;
+
+
+        public int HeightTime { get; set; }
+        public int Height { get; set; }
+
+    }
+
+    public class SecondEntity : BaseEntity
+    {
+        public double MpuTime {
+            get { return _mpu_time; }
+            set {
+                _mpu_time = value;
+                onPropertyChanged(nameof(MpuTime));
+            }
+        }
+        private double _mpu_time;
+
+        public double MpuRoll {
+            get { return _mpuRoll; }
+            set {
+                _mpuRoll = value;
+                onPropertyChanged(nameof(MpuRoll));
+            }
+        }
+        private double _mpuRoll;
+
+        public double MpuPitch {
+            get { return _mpuPitch; }
+            set {
+                _mpuPitch = value;
+                onPropertyChanged(nameof(MpuPitch));
+            }
+        }
+        public double _mpuPitch;
+
+        public double MpuYaw {
+            get { return _mpuYaw; }
+            set {
+                _mpuYaw = value;
+                onPropertyChanged(nameof(MpuYaw));
+            }
+        }
+        public double _mpuYaw;
+
+
+        public int GpsTime { get; set; }
         public int Latitude { get; set; }
         public int Longitude { get; set; }
-        public int Height { get; set; }
-        public string MpuRYaw { get; set; } = "0";
-        public string MpuRRoll { get; set; } = "0";
-        public string MpuXYaw { get; set; } = "0";
-        public string MpuYRoll { get; set; } = "0";
-        public string RollInput { get; set; } = "0";
-        public string DrugR { get; set; } = "0";
-        public string PitchInput { get; set; } = "0";
-        public string DrugL { get; set; } = "0";
+    }
 
-        public string VoltageR { get; set; } = "0";
+    public class ThirdEntity : BaseEntity
+    {
+        public double ControlTime { get; set; }
+        public double RollInput { get; set; }
+        public double DrugR { get; set; }
+        public double PitchInput { get; set; }
+        public double DrugL { get; set; }
+        public double MpuRYaw { get; set; }
+        public double MpuRRoll { get; set; }
+    }
 
-        public string VoltageL { get; set; } = "0";
-        #endregion
+    public class ForthEntity : BaseEntity
+    {
+        public double ControlTime { get; set; }
+
+        public double MpuXYaw { get; set; }
+        public double MpuYRoll { get; set; }
+
+
+        public double VoltageR { get; set; }
+
+        public double VoltageL { get; set; }
+    }
+
+    public abstract class BaseEntity : INotifyPropertyChanged
+    {
+        public double StartUnixTime {
+            get { return _start_unix_time; }
+            set {
+                _start_unix_time = value;
+                onPropertyChanged(nameof(StartUnixTime));
+            }
+        }
+        private double _start_unix_time;
+
+        public T Clone<T>() {
+            return (T)MemberwiseClone();
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        private void onPropertyChanged(String propertyName = "") {
+        protected void onPropertyChanged(string propertyName = "") {
             if (PropertyChanged != null) {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
-        #endregion
     }
+    /// <summary>
+    /// ここに新しい要素を追加するときは、database.sqlにも項目を追加してください
+    /// </summary>
 
     #region 設定用クラス
     /// <summary>
@@ -117,20 +162,20 @@ namespace AlbaAnalysis.Entity
 
     public class portNames
     {
-        public string portName { get; set; }
+        public double portName { get; set; }
     }
     /// <summary>
     /// データを保存したパスを保持
     /// </summary>
     public class filePath
     {
-        public string pathName { get; set; }
+        public double pathName { get; set; }
     }
 
     public class DetailEntity
     {
-        public string value { get; set; }
-        public string time { get; set; }
+        public double value { get; set; }
+        public double time { get; set; }
     }
     #endregion
 }
